e0e48e3d03e880e30315c435635b4f05ed46baff
BrunodaSilvaMachado <brunosilvamachado@id.uff.br>
2023-05-07 08:13:34 -0300

    drag and drop matching game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { IPin, OneMatch, pins, pinshover } from './model/pin';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dragndrop-matching-game';
+
+  todo: IPin[] = this.shuffleArray(pins);
+
+  done: OneMatch[] = this.shuffleArray(pinshover);
+
+  drop(event: CdkDragDrop<IPin[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+
+  matchPredicate(item: CdkDrag<IPin>, list: CdkDropList<IPin[]>) {
+    return list.data.map((pin)=>pin.id).includes(item.data.id);
+  }
+
+  /** Predicate function that doesn't allow items to be dropped into a list. */
+  noReturnPredicate() {
+    return false;
+  }
+
+  shuffleArray<T>(inputArray: T[]){
+    return inputArray.sort(()=> Math.random() - 0.5);
+  }
 }
